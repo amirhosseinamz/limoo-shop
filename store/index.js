793a24d3a1b12dea273d6d085afc433e3a54517b
Mohammad Solgi <mohammadsolgi96@gmail.com>
2021-02-14 09:20:08 +0330

    change store- classic store will be depricated in NUXT 3

--- store/index.js
+++ store/index.js
@@ -1,29 +1,61 @@
-import Vuex from "vuex";
-// import { createStore } from "./../.nuxt/store";
-// import { createStore } from "vuex";
-import rootMutations from "./mutations.js";
-import rootActions from "./actions.js";
-import rootGetters from "./getters.js";
-//it should be callable by nuxt which will be executed on the server to setup the store
-const createStore = () => {
-    /* if we returned an object instead of the function then all users of our app would actually 
+/* if we returned an object instead of the function then all users of our app would actually
   get the same instance because on the server in node where does runs. if we have plain object
-  stored well then we always get that object for every connected user. if we have a function 
+  stored well then we always get that object for every connected user. if we have a function
   that returns and you store as this does then every new user recives his owen store*/
-    return new Vuex.Store({
-        // namespaced: true, // tell vuex now the entire module shuld be kind of deatached from the rest of the store
-        state: {
-            signUp: "stepOne",
-            signIn: "stepOne",
-            phone: "",
-            password: "",
-            userAuth: false,
-            showModalWellcome: false
-        },
 
-        mutations: rootMutations,
-        actions: rootActions,
-        getters: rootGetters
-    });
+export const state = () => ({
+    // signUp: "stepOne",
+    // signIn: "stepOne",
+    phone: "",
+    // password: "",
+    userAuth: false,
+    showModalWellcome: false
+});
+
+export const mutations = {
+    walkInSignUpcomponents(state, payload) {
+        state.signUp = payload.value;
+    },
+    walkInSignIncomponents(state, payload) {
+        state.signIn = payload.value;
+    },
+    PhoneNumber(state, inputPhone) {
+        state.phone = inputPhone.value;
+    },
+    passHolder(state) {
+        return state.password;
+    },
+    userIsAuth(state, payload) {
+        state.userAuth = payload.value;
+    },
+    stateShowModalWellcome(state, payload) {
+        state.showModalWellcome = payload.value;
+        // console.log("showModalWellcome in store is", payload.value);
+    }
 };
-export default createStore;
+export const actions = {
+    userIsAuth(context, payload) {
+        context.commit("userIsAuth", payload);
+    },
+    stateShowModalWellcome(context, payload) {
+        context.commit("stateShowModalWellcome", payload);
+    }
+};
+
+export const getters = {
+    signUpPicker(state) {
+        return state.signUp;
+    },
+    signInPicker(state) {
+        return state.signIn;
+    },
+    PhoneNumberPicker(state) {
+        return state.phone;
+    },
+    CurentRoute(state) {
+        return state.CurentRoute;
+    },
+    stateShowModalWellcome(state) {
+        return state.showModalWellcome;
+    }
+};
